b61df0d8d6015313b057555f7f5bc6f14161967d
Venicius Santana <dev.venicius1912@gmail.com>
2023-11-22 20:22:56 -0300

    Adicionado as funções dos botões

--- src/main.py
+++ src/main.py
@@ -8,6 +8,18 @@ class App(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.btn_pg_cadastro.clicked.connect(self.vi_tela_cad)
+        self.btn_home.clicked.connect(self.vi_tela_home)
+        
+    def vi_tela_cad(self):
+        self.pages.setCurrentIndex(2)
+        
+    
+    def vi_tela_home(self):
+        self.pages.setCurrentIndex(1)
+    
+    
+        
 
 
 if __name__ == '__main__':
